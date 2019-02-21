bb3a780aa21b4f01e2f6f0de469ab91db70bcf53
Martin Horgan <martinhorgan@ucdconnect.ie>
2019-02-21 18:03:33 +0000

    Update 21-02-19

--- dublinRabbitBikes.py
+++ dublinRabbitBikes.py
@@ -1,31 +1,34 @@
 # dublinRabbitBikes
+import requests
+import json
+from pprint import pprint
+import time
 
 """
 Program to scrape DublinBikes data.
 
 Rough first attempt
 """
-NAME="Dublin"                                           # name of contract
-STATIONS="https://api.jcdecaux.com/vls/v1/stations"     # and the JCDecaux endpoint
-APIKEY = "secret"
-def main():
-                                                        # run forever...
-        while True:
-                try:
-                    r = requests.get(STATIONS,
-                         params={"apiKey": APIKEY, "contract": NAME})
-                    store(json.loads(r.text))                                # now sleep for 5 minutes
-                        time.sleep(5*60)
-                except:
-                                                        # if there is any problem, print the traceback
-                        print traceback.format_exc()
-        return
+NAME="Dublin"                                             # name of contract
+STATIONS_URI="https://api.jcdecaux.com/vls/v1/stations"       # and the JCDecaux endpoint
+APIKEY ="1b5a70bba36e98ed68efe695e839bcb44f4e6e27"
+
+# r = requests.get("{}/{}".format(STATIONS_URI, 41),
+#                  params={"apiKey": APIKEY, "contract": NAME})
+# pprint(json.loads(r.text))
+
+
+def main():                                             # run forever...
+                                                         
+    while True:
+        try:
+            r = requests.get(STATIONS_URI, params = {"apiKey": APIKEY, "contract": NAME})
+            print(json.loads(r.text))                 
+            time.sleep(5*60)                                # now sleep for 5 minutes
+        except:
+                print("error")
+#             print(traceback.format_exc())               # if there is any problem, print the traceback
+    return
                 
 
-
-
-                      
-
-
-
-
+main()
